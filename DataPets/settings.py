a05a9946f491c4e83688afd17ab60cf3b1e5c4c2
blanca-marlen <blancapidiache274@gmail.com>
2022-09-02 13:32:51 -0500

    configuraciones básicas terminadas

--- DataPets/settings.py
+++ DataPets/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 
 STATIC_URL = 'static/'
 
