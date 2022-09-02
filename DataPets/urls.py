868934c0e60f5da27e3344fd1aa8e7716413ca54
blanca-marlen <blancapidiache274@gmail.com>
2022-09-02 07:51:16 -0500

    se cambió la estructura de las aplicaciones

--- DataPets/urls.py
+++ DataPets/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls', namespace='main')),
+    path('', include('apps.main.urls', namespace='main')),
     path('admin/', admin.site.urls),
 ]
