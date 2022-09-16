9a717befceb36e275d55b3c6ba44787c899d1f05
blanca-marlen <blancapidiache274@gmail.com>
2022-09-16 17:46:23 -0500

    login básico

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,54 @@
-from django.shortcuts import render
+from email import message
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
-def sign_in(request):
-    return render(request, 'user/sign-in.html')
+def user(request):
+    return render(request, 'user/user.html')
 
 def sign_up(request):
-    return render(request, 'user/sign-up.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            password = password1
+        else:
+            message.info(request, 'Las contraseñas no coinciden')
+            return redirect('/')
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Nombre de usuario ya está en uso')
+            return redirect('/')
+        elif User.objects.filter(email=email).exists():
+            messages.info(request, 'Correo ya está en uso')
+            return redirect('/')
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            user = auth.authenticate(username=username, password=password)
+            auth.login(request, user)
+            return redirect('user/')
+    else:
+        return render(request, '/')
+
+def sign_in(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('user/')
+        else:
+            messages.info(request, 'Nombre de usuario o contraseña inválido')
+            return redirect('/')
+    else:
+        return render(request, '/')
+
+def logout_user(request):
+    auth.logout(request)
+    return redirect('/')
