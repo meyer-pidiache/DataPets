f1c76950efb5246858067530d46aad671ccc270d
blanca-marlen <blancapidiache274@gmail.com>
2022-09-17 21:41:34 -0500

    acerca de nosotros

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,4 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/index.html')
+    context = {'title': 'DataPets'}
+    return render(request, 'main/index.html', context)
+
+def about_1(request):
+    context = {'title': '¿Qué es DataPets?'}
+    return render(request, 'main/about_1.html', context)
+
+def about_2(request):
+    context = {'title': '¿Dónde encontrarnos?'}
+    return render(request, 'main/about_2.html', context)
+
+def about_3(request):
+    context = {'title': 'Contáctenos'}
+    return render(request, 'main/about_3.html', context)
