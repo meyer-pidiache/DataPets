2be1fbfe4ebfada816e479b5244e9b9e65f7dfa5
blanca-marlen <blancapidiache274@gmail.com>
2022-09-27 08:12:36 -0500

    user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Gender(models.Model):
+    description = models.CharField(max_length = 12, null=True, blank=True)
+
+    def __str__(self):
+        return self.description
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phoneNumberRegex = RegexValidator(regex = r"^\+?57?\d{8,15}$")
+    phone_number = models.CharField(validators = [phoneNumberRegex], max_length = 16, null=True, blank=True)
+    gender = models.ManyToManyField(Gender)
+    profile_picture = models.ImageField(upload_to ='static/assets/img/profiles/', null=True, blank=True)
