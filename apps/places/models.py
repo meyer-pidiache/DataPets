a0855ebc42222355f730e30ec0c59002460dbac2
blanca-marlen <blancapidiache274@gmail.com>
2022-09-30 17:47:04 -0500

    formulario de lugares

--- apps/places/models.py
+++ apps/places/models.py
@@ -2,20 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from PIL import Image
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Place(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
     place_name = models.CharField(max_length = 50)
     place_direction = models.CharField(max_length = 50)
     phoneNumberRegex = RegexValidator(regex = r"^\+?57?\d{8,15}$")
     place_phone_number = models.CharField(validators = [phoneNumberRegex], max_length = 16, null=True, blank=True)
     place_opening_hours = models.CharField(max_length = 50)
     place_visit_date = models.DateField(null=True, blank=True)
-    place_logo = models.ImageField(upload_to='media/places/',
-                                        default='media/places/default.jpg', 
+    place_logo = models.ImageField(upload_to='media/places/logo',
+                                        default='media/places/logo/default.jpg', 
                                         null=True, blank=True, editable=True)
-    place_photo = models.ImageField(upload_to='media/places/',
-                                        default='media/places/default.jpg', 
+    place_photo = models.ImageField(upload_to='media/places/photo',
+                                        default='media/places/photo/default.jpg', 
                                         null=True, blank=True, editable=True)
 
     place_logo_height = models.PositiveIntegerField(null=True, blank=True, editable=False, default="40")
@@ -50,3 +53,6 @@ class Place(models.Model):
         size = ( 200, 200)
         image = image.resize(size, Image.ANTIALIAS)
         image.save(self.place_photo.path)
+
+    def __str__(self):
+        return self.place_name
