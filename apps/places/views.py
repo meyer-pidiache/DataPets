04296fc3cb10700319b2e8f349b9eb3be3d94043
blanca-marlen <blancapidiache274@gmail.com>
2022-10-01 08:04:33 -0500

    modelo de lugares renombrado

--- apps/places/views.py
+++ apps/places/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+from .models import Place
 from .forms import PlaceForm
 
 def places(request):
+    places = Place.objects.order_by('-visit_date')[:18]
     form = PlaceForm()
     if request.method == 'POST':
         form = PlaceForm(request.POST or None,
@@ -18,5 +20,6 @@ def places(request):
         else:
             messages.info(request, 'Información inválida')
 
-    context = {'form': form}
+    context = {'form': form,
+               'places': places}
     return render(request, 'places/places.html', context)
