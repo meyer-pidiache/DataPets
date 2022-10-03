d82785fdcc5a5098cf9c08983aeba47f02813d9d
blanca-marlen <blancapidiache274@gmail.com>
2022-10-02 21:28:15 -0500

    media hosting

--- DataPets/settings.py
+++ DataPets/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'apps.user.apps.UserConfig',
     'apps.places.apps.PlacesConfig',
 
+    'cloudinary_storage',
+    'cloudinary',
     'crispy_forms',
 ]
 
@@ -147,9 +149,18 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# Media settings
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': dec('CLOUD_NAME'),
+    'API_KEY': dec('API_KEY'),
+    'API_SECRET': dec('API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+CLOUDINARY_URL = dec('CLOUDINARY_URL')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- apps/user/signals.py
+++ apps/user/signals.py
@@ -10,4 +10,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    try:
+        instance.profile.save()
+    except:
+        pass
